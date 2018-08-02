20f73856c9ada73cb6ba97340ac94362f27277d3
subuta <shunta.3110@gmail.com>
2018-08-02 22:15:42 +0900

    Add initial files.

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # safari-push-example
 Node.js server &amp; browser client example of Safari Push Notifications using APNS.
+
+### Prerequisites
+
+- [Active Membership of iOS Developer Account](https://developer.apple.com/account/#/overview/4579AZ8Q4Z)
+
+### How to develop
+
+```
+# 1. Clone this repo.
+
+git clone https://github.com/subuta/safari-push-example
+
+# 2. Download latest certificate from apple and put it to root of this repo.
+
+wget https://developer.apple.com/certificationauthority/AppleWWDRCA.cer
+
+# 3. Create development certificate on http://developer.apple.com
+# 3-1. Login to http://developer.apple.com
+
+# 3-2-1. Then navigate to https://developer.apple.com/account/
+# 3-2-2. Click `Certificates, IDs & Profiles` on the left pane.
+# 3-2-3. Go to `App IDs` of Identifiers section, and click plus icon to add id.
+# 3-2-4. Fill these fields, and click `Continue`
+# - App ID Description
+# - Choose `Explicit App ID` and put Bundle ID
+# - Enable `Push Notifications` at App Services section.
+# 3-2-5. At confirm page, make sure `Push Notifications` are enabled(become Configurable), and then click `Register`
+# 3-2-6. Then click `Done` to finish adding App ID.
+
+# 3-3-1. Go to `Development` of Certificates section, and click plus icon to add id.
+# 3-3-2. Fill these fields, and click `Continue`
+# - Choose `Apple Push Notification service SSL (Sandbox)` of Development section.
+# 3-3-3. At App ID selection, choose App ID created at previous step, and click `Continue`.
+# 3-3-4. Follow the shown steps and Create a Certificate Signing Request (CSR)*[]:
+# SEE: https://help.apple.com/developer-account/#/devbfa00fef7
+# 3-3-5. Click `Choose File...` and select CSR file(created at previous step), and click `Continue`.
+# 3-3-6. Click `Download` button for download your certificate :)
+
+# 4. Install development certificate and export as a .p12 File
+# 4-1. Double click cerificate file downloaded at step #3-3-6
+# 4-2. Find installed cerificate(Named like `Apple Development IOS Push Services: ${Bundle ID}`) at keychain.
+# 4-3. Drag and drop it to `login` keychain if it exists at `System`
+# SEE: https://stackoverflow.com/a/21060610
+# 4-4. Right click cerificate, and select `Export "..."`
+# 4-5. Select `Personal Information Excange(.p12)` at File Format, and click `Save`.
+# 4-6. Fill Password/Verify and. click `OK` for save file.
+```
---
+++ generate_pushpackage.js
@@ -0,0 +1,99 @@
+// Borrowed from https://github.com/wingleung/safari-push-serverside
+
+import 'dotenv'
+import fs from 'fs'
+import crypto from 'crypto'
+import childProcess from 'child_process'
+import rimraf from 'rimraf'
+import archiver from 'archiver'
+
+const {
+  WEBSITE_NAME,
+  WEBSITE_PUSH_ID,
+  ALLOWED_DOMAIN,
+  URL_FORMAT_STRING,
+  AUTHENTICATION_TOKEN,
+  WEB_SERVICE_URL,
+  CERT_P12_FILENAME,
+  CERT_P12_PASSWORD
+} = process.env
+
+let rawFiles = [
+  'icon.iconset/icon_16x16.png',
+  'icon.iconset/icon_16x16@2x.png',
+  'icon.iconset/icon_32x32.png',
+  'icon.iconset/icon_32x32@2x.png',
+  'icon.iconset/icon_128x128.png',
+  'icon.iconset/icon_128x128@2x.png'
+]
+
+const website = {
+  'websiteName': WEBSITE_NAME,
+  'websitePushID': WEBSITE_PUSH_ID,
+  'allowedDomains': ALLOWED_DOMAIN,
+  'urlFormatString': URL_FORMAT_STRING,
+  // any token to auth user
+  'authenticationToken': AUTHENTICATION_TOKEN,
+  'webServiceURL': WEB_SERVICE_URL
+}
+
+const manifest = {}
+const pushPackagePath = `${__dirname}/safari-push-example.pushpackage`
+
+if (fs.existsSync(pushPackagePath)) {
+  rimraf.sync(pushPackagePath)
+}
+
+fs.mkdirSync(pushPackagePath)
+fs.mkdirSync(pushPackagePath + '/icon.iconset')
+
+rawFiles.forEach((file) => {
+  console.log('file = ', file)
+  childProcess.execSync(`cp '${__dirname}/${file}' '${pushPackagePath}/${file}'`)
+})
+
+console.log('website.json')
+fs.writeFileSync(pushPackagePath + '/website.json', JSON.stringify(website))
+
+console.log('manifest.json')
+rawFiles.push('website.json')
+
+rawFiles.forEach((file) => {
+  const sha1 = crypto.createHash('sha1')
+  sha1.update(fs.readFileSync(pushPackagePath + '/' + file), 'binary')
+  manifest[file] = sha1.digest('hex')
+})
+
+fs.writeFileSync(pushPackagePath + '/manifest.json', JSON.stringify(manifest))
+
+childProcess.execSync(`openssl pkcs12 -in '${CERT_P12_FILENAME}' -nocerts -out 'private.pem' -passin pass:${CERT_P12_PASSWORD} -passout pass:${CERT_P12_PASSWORD}`)
+childProcess.execSync(`openssl pkcs12 -in '${CERT_P12_FILENAME}' -clcerts -nokeys -out 'cert.pem' -passin pass:${CERT_P12_PASSWORD}`)
+childProcess.execSync(`openssl smime -binary -sign -certfile AppleWWDRCA.pem -signer cert.pem -inkey private.pem -in '${pushPackagePath}/manifest.json' -out '${pushPackagePath}/signature' -outform DER -passin pass:${CERT_P12_PASSWORD}`)
+
+rawFiles.push('manifest.json')
+rawFiles.push('signature')
+
+if (fs.existsSync(pushPackagePath + '.zip')) {
+  fs.unlink(pushPackagePath + '.zip')
+}
+
+const output = fs.createWriteStream(pushPackagePath + '.zip')
+const archive = archiver('zip')
+
+archive.pipe(output)
+
+console.log('building Package')
+
+rawFiles.forEach((file) => {
+  archive.append(fs.createReadStream(pushPackagePath + '/' + file), {name: file})
+})
+
+archive.finalize(function (err, bytes) {
+  if (err) throw err
+
+  console.log(`Total bytes: ${bytes} written.`)
+
+  fs.unlink('cert.pem')
+  fs.unlink('private.pem')
+  rimraf.sync(pushPackagePath)
+})
